337603c95567df8d1e0d879b4d770bfbb787cfb2
alexander122d <alexander122d@gmail.com>
2019-06-14 09:17:19 +0500

    issue download bug fixes

--- pages/issue/IssueHandler.inc.php
+++ pages/issue/IssueHandler.inc.php
@@ -5,6 +5,12 @@ class MlurlIssueHandler extends IssueHandler {
   function initialize($request, $args = array()) {
     unset($args[0]);
     $args = array_values($args);
+    $journal = $request->getContext();
+    $issueDao = DAORegistry::getDAO('IssueDAO');
+    $issue = $issueDao->getByBestId((int) $journal->getId(), $args[0], true);
+    $request->getRouter()->_page = 'issue';
+    $request->getRouter()->_op = 'view';
+    $request->getRouter()->getHandler()->_authorizationDecisionManager->_authorizedContext[ASSOC_TYPE_ISSUE] = $issue;
     parent::initialize($request, $args);
   }
   function view($args, $request) {
